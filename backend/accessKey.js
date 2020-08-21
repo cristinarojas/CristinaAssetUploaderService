146b9c5c9b7e3766463d38947a7c1bb669031762
Cristina Rojas <telematicacristina@gmail.com>
2020-08-21 14:34:58 -0700

    Adding the initial structure of the backend project

---
+++ backend/accessKey.js
@@ -0,0 +1,9 @@
+// cristinaassetuploaderservice
+
+// region=us-west-1
+
+// Access Key ID:
+// AKIAJAJEHNVQCBHY47DA
+
+// Secret Access Key:
+// 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
---
+++ backend/index.js
@@ -0,0 +1,54 @@
+
+// Dependencies
+require('dotenv/config'); // To store our keys
+const express = require('express'); // Importing express from express
+const multer = require('multer'); // Middleware to habdle the endpoint
+const AWS = require('aws-sdk'); // Importing the aws sdk to handle the code for save the image
+const uuid = require('uuid').v4; // To handle repeated image names 
+
+// Creating the server with the express library
+// Server configuration
+const app = express(); // App from express 
+const port = 3000; // The port where the app will be running 
+
+// Aws Configuration object storage service
+const s3 = new AWS.S3({ // Amazon Simple Storage Service (Amazon S3) 
+    accessKeyId: process.env.ACCESS_KEY_ID, // Given by Amazon 
+    secretAccessKey: process.env.SECRET_ACCESS_KEY // Given by Amazon
+});  
+
+// Multer configuration (middleware)
+const storage = multer.memoryStorage({
+    destination: function(req, file, callback) {
+        callback(null, ''); // where the files will be saved 
+    }
+});
+
+const upload = multer({storage}).single('image'); // Middleware setup
+
+// Path's
+app.post('/upload', upload, (req, res) => {
+
+    // File details
+    let myFile = req.file.originalname.split('.'); // Getting the originalname string into an array to get the info 
+    const fileType = myFile[myFile.length - 1]; // Getting the file extension (las position of the array)
+    const params = { // This object have information that the s3.upload need to be executed
+        Bucket: process.env.BUCKET_NAME,
+        Key: `${myFile[0]}.${fileType}`,
+        Body: req.file.buffer
+    }
+
+    // Execution the upload function that is an instance of s3
+    s3.upload(params, (error, data) => {
+        if(error) {
+            res.status(500).send(error);
+        }
+
+        res.status(200).send(data);
+    });
+});
+
+// Running the server
+app.listen(port, () => {
+    console.log(`Server us up at this port port: ${port}`); // Printing the server port when is running
+});
