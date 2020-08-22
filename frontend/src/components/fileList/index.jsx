c516b264de40a5829bd6f7b9dca43c7a9ed6065e
Cristina Rojas <telematicacristina@gmail.com>
2020-08-21 21:02:14 -0700

    Adding uploaded component to front end project

---
+++ frontend/src/components/fileList/index.jsx
@@ -0,0 +1,19 @@
+// Dependencies
+import React, { useState } from 'react';
+
+// Styles 
+import './fileList.scss';
+
+const Filelist = ({ filesInS3 }) => {
+
+    console.log('filesInS3 --->', filesInS3)
+
+  return ( 
+    <section className="fileList">
+        <h2>Current files in S3: </h2>
+        <span>{filesInS3}</span>
+    </section>
+  )
+}
+
+export default Filelist;
--- frontend/src/components/uploader/index.jsx
+++ frontend/src/components/uploader/index.jsx
@@ -1,55 +1,77 @@
 // Dependencies
 import React, { useState } from 'react';
 
+// Components
+import Filelist from '../fileList'
+
 // Styles 
 import './uploader.scss';
 
 
 const Uploader = () => {
   // Local State
-  const [stept, setStep] = useState('Select file');
+  const [showImage, setShowImage] = useState(false);
+  const [ filesInS3, setFilesInS3] = useState(0);
 
   // Handle image
-  const loadFile = (event) => {
+  const loadFile = (e) => {
     let image = document.getElementById('output');
-    image.src = URL.createObjectURL(event.target.files[0]);
-    setStep('Upload file to S3');
+    let binaryData = [];
+
+    binaryData.push(e.target.files[0]); 
+    image.src = window.URL.createObjectURL(new Blob(binaryData, {type: "application/zip"})) 
+
+    setShowImage(true);
   };
 
   return ( 
-    <section className="uploaderContainer">
+    <section className="mainContainer">
 
-      <section className="selectorContainer">
-        {
-          stept === 'Select file' ? (
-            <>
-              <label htmlFor="fileUpload" className="customFileUpload">
-                <i className="fa fa-cloud-upload"></i> {stept}
-              </label>
-              <input 
-                id="fileUpload" 
-                type="file" 
-                accept="image/*" 
-                name="image"  
-                onChange={(e) => loadFile(e)}
-              />
-            </>
-          ) : (
-            <label htmlFor="fileUpload" className="customFileUpload">
-              <i className="fa fa-cloud-upload"></i>  {stept}
-            </label>
-          )
-        }
-
-      </section>
-
-      <section className="imageContainer">
-        <img 
-        className="outputFile"
-          id="output" 
+      <section className="allFileContainer">
+        <Filelist 
+          filesInS3={filesInS3} 
         />
       </section>
-  
+
+      <section className="uploaderContainer">
+        <section className="selectorContainer">
+          <label htmlFor="fileUpload" className="customFileUpload">
+            <i className="fa fa-cloud-upload"></i> Select file
+          </label>
+          <input 
+            id="fileUpload" 
+            type="file" 
+            accept="image/*" 
+            name="image"  
+            onChange={(e) => loadFile(e)}
+          />
+          
+          {
+            showImage ? (
+              <label htmlFor="fileUpload" className="uploadAWS">Upload to S3
+            </label>
+            ) : ''
+          }
+
+        </section>
+
+
+        <section className="imageContainer">
+          {
+            showImage ? (
+            <label className="fileInfo">Cristinawiththefish.jpg</label>
+            ) : ''
+          }
+          <img 
+            className="outputFile"
+            id="output" 
+          />
+        </section>
+      </section>
+
+
+
+
     </section>
   )
 }
