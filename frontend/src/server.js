876033963473cb766c821b308525fcb47f65d8b6
Cristina Rojas <telematicacristina@gmail.com>
2020-08-23 13:37:48 -0700

    Fixing __dirname

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -14,6 +14,7 @@ import clientRender from './render/clientRender.js'
 
 // Environment
 const isProduction = process.env.NODE_ENV === 'production'
+const __dirname = path.resolve()
 
 // Express Application
 const app = express()
