9b159fdf222af31ad88f7d02a444ca4783f7cf95
Cristina Rojas <telematicacristina@gmail.com>
2020-08-23 13:33:55 -0700

    Fixing __dirname

--- frontend/webpack/configuration/output.js
+++ frontend/webpack/configuration/output.js
@@ -1,9 +1,11 @@
 // Dependencies
-import path from 'path';
+import path from 'path'
+
+const __dirname = path.resolve()
 
 // This will be the path for the output webpack files (bundles)
 export default {
   filename: '[name].js',
   path: path.resolve(__dirname, '../../public/app'),
   publicPath: '/'
-};
+}
